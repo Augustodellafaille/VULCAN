afd443a071fa2b18ea004134c834e62d8e7e46ca
Augusto della Faille <augusto.dellafaille@gmail.com>
2017-01-20 11:37:11 +0100

    typo correct

--- db/migrate/20170116153613_create_users.rb
+++ db/migrate/20170116153613_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :surname
       t.string :address
-      t.string :email
+      # t.string :email
 
       t.timestamps
     end
